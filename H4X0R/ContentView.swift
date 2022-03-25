f04575d0f51f124315e3e5911291d0ea1cd48a76
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-25 17:55:07 +0200

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Eetu Hernesniemi on 25.3.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Eetu Hernesniemi on 25.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
